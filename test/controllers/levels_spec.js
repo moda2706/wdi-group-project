8aedff0581ebc0ddbc4b6018d8dda006bdab0327
moda2706 <monju.miah27@gmail.com>
2017-05-14 13:49:49 +0100

    first test passing

---
+++ test/controllers/levels_spec.js
@@ -0,0 +1,9 @@
+require('../spec_helper');
+
+describe('Levels Controller Test', () => {
+  describe('GET /api/levels', () => {
+    it('should return a 200 response', done => {
+      done();
+    });
+  });
+});
