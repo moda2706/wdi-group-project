6efc5ace62aa4aefa01daf7816e9c7c5b9311ed2
kartuzovmax <kartuzovmax@hotmail.com>
2017-05-16 14:15:34 +0500

    Typing thing working

--- src/js/controllers/show-index.controller.js
+++ src/js/controllers/show-index.controller.js
@@ -1,52 +1,144 @@
 angular
 .module('qwertyApp')
-.controller('ShowIndexCtrl', ShowIndexCtrl);
+.controller('ShowIndexCtrl', ShowIndexCtrl)
+.directive('keypressEvents', KeyPressDirective);
 
-ShowIndexCtrl.$inject = ['Level', '$stateParams', '$scope'];
+function KeyPressDirective($document, $rootScope) {
+  return {
+    restrict: 'A',
+    link: function () {
+      console.log('linked');
+      $document.bind('keypress', function (e) {
+        $rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
+      });
+    }
+  };
+}
 
-function ShowIndexCtrl(Level, $stateParams, $scope) {
+ShowIndexCtrl.$inject = ['Level', '$stateParams', '$scope', '$rootScope'];
+
+function ShowIndexCtrl(Level, $stateParams, $scope, $rootScope) {
 
   const vm = this;
   vm.text = 'test test test';
   vm.level = Level.get($stateParams);
   console.log($stateParams);
 
+
+  // Preventing TAB
+  $(document).keydown(function(objEvent) {
+    if (objEvent.keyCode === 9) {  //tab pressed
+      objEvent.preventDefault(); // stops its action
+    }
+  });
+
+  // Getting our Level Data
   Level.get({ id: $stateParams.id}).$promise.then(data => {
     vm.level = data;
     console.log('Level is ');
     console.log(vm.level.content);
     console.log('hi');
+    // Wrapping original text into spans and setting it back to the DOM
+    $scope.$textSource = $('#textSource').html(splitIntoSpans(vm.level.content));
+    $scope.$textSource.find('span').eq(0).addClass('next');
   });
 
+  // Function which wraps each char into a span
+  function splitIntoSpans(text) {
+    return text.split('').map(function(n) {
+      return '<span>' + n + '</span>';
+    }).join('');
+  }
 
-
+  // function which runs each time user enters a character
   $scope.output = function() {
 
-
-    const charIndex = $scope.textInput.length;
-    const originalText = vm.level.content.substring(0,charIndex);
     const inputText = $scope.textInput;
+    const charIndex = inputText.length-1;
+    const originalText = vm.level.content.substring(0,$scope.textInput.length);
 
     console.log(inputText);
+    var spanClass;
 
-    // Checking code for mistakes
-    if (originalText === inputText) {
+    // Checking input for mistakes
+    if (originalText[charIndex] === inputText[charIndex]) {
       console.log('Correct');
       // So far the input text is correct. Check for win condition:
       $scope.$textInput = $('#textInput').css({
         color: 'green'
       });
 
-      if(inputText.length === vm.level.content.length) {
+      spanClass = 'correct';
+
+      if(inputText === vm.level.content) {
         // User wins!
-        console.log('Win!');
+        alert('You won!');
       }
+
     } else {
+
       console.log('Incorrect');
+      console.log(`Original text is ${originalText} and input text is ${inputText}`);
       $scope.$textInput = $('#textInput').css({
         color: 'red'
       });
+
+      spanClass = 'wrong';
     }
+
+    const allSpans = $scope.$textSource.find('span');
+    const currentSpan = allSpans.eq([charIndex]);
+
+    // Removing all classes from next spans
+    // for (let i = vm.level.content.length-1; i > charIndex; i-- ) {
+    //   console.log('Entered the loop');
+    //   const nextSpan = allSpans.eq(i);
+    //   nextSpan.removeClass();
+    // }
+    currentSpan.removeClass();
+    if (inputText.length !== 0) {
+      currentSpan.addClass(spanClass);
+      currentSpan.nextAll().removeClass();
+      currentSpan.next().addClass('next');
+    } else {
+      allSpans.removeClass();
+      allSpans.eq(0).addClass('next');
+    }
+
   };
+
+  // $scope.key = 'none';
+
+  // Key Listener 0
+  // $rootScope.$on('keypress', function (evt, obj, key) {
+  //   $scope.$apply(function () {
+  //
+  //     if(evt.keyCode === 8) {
+  //       console.log('Delete');
+  //     }
+  //     $scope.key = key;
+  //     console.log($scope.key);
+  //   });
+  // });
+
+  //function addKeyPressHandler() {
+
+  //   $(document).on('keypress', function(event) {
+  //
+  //     var input = String.fromCharCode(event.charCode),
+  //     isRight = input === currentLesson[counter],
+  //     spanClass = isRight ? "right" : "wrong",
+  //     allSpans = chars.find("span"),
+  //     currentSpan = allSpans.eq(counter);
+  //
+  //     counter++;
+  //     once || (once = true) && output.text("");
+  //     allSpans.removeClass("underline");
+  //     currentSpan.addClass(spanClass);
+  //     currentSpan.next().addClass(" underline");
+  //     output.text(output.text() + input);
+  //     event.preventDefault();
+  //   });
+  // }
 
 }
