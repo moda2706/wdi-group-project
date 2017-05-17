7476a6c75fa39125d8f1732cda33f4d14bfc7e0f
moda2706 <monju.miah27@gmail.com>
2017-05-17 17:07:34 +0100

    Error handler

--- src/js/controllers/authentications/register.controller.js
+++ src/js/controllers/authentications/register.controller.js
@@ -5,8 +5,10 @@ angular
 RegisterCtrl.$inject = ['User', 'CurrentUserService', '$state'];
 function RegisterCtrl(User, CurrentUserService, $state){
   const vm = this;
+  vm.emailValidate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   console.log(vm.user);
   vm.register = () => {
+
 
     User
       .register(vm.user)
