d4886816f95801f45bc5993599b35c2e42557ebb
moda2706 <monju.miah27@gmail.com>
2017-05-19 10:58:10 +0100

    gettting read for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+./node_modules/.bin/forever -m 5 index.js
--- config/env.js
+++ config/env.js
@@ -1,5 +1,9 @@
 module.exports = {
+  db: {
+    production: process.env.MONGODB_URI,
+    development: 'mongodb://localhost:27017/qwertyApp-development',
+    test: 'mongodb://localhost:27017/qwertyApp'
+  },
   port: process.env.PORT || 4000,
-  db: 'mongodb://localhost:27017/qwertyApp',
   secret: process.env.SECRET || 'gosh this is so secret... shhh...'
 };
